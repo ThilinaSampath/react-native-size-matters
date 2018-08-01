c8bc2d634ca057896da203d19e7fa173594fe1e1
Niordha Perera <nperera@mitrai.com>
2018-08-01 14:12:07 +0530

    Added support for orientation styles.

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export { default as ScaledSheet } from './lib/ScaledSheet';
 export { moderateScale, scale, verticalScale } from './lib/scalingUtils';
+export { getStyleByOrientation } from './lib/getStyleByOrientation';
